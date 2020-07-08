01581d53f789dcd228b136ff7563142f6547cbee
Meco <920369182@qq.com>
2020-07-08 16:24:23 +0800

    Signed-off-by: Meco

--- rt_thread_3.1.3-ucosiii_3.03-wrapper/uCOS-III_Wrapper/uCOS-III/os_task.c
+++ rt_thread_3.1.3-ucosiii_3.03-wrapper/uCOS-III_Wrapper/uCOS-III/os_task.c
@@ -49,7 +49,6 @@
 *                   OSTaskQFlush
 *                   OSTaskQPendAbort
 *                   OSTaskSemPendAbort
-*                   OSTaskSemSet
 *                   OSTaskChangePrio
 *                   OSTaskTimeQuantaSet
 ************************************************************************************************************************
