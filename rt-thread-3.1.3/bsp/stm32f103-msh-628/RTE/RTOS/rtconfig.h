bfee9a0b9531889958b707d851834b6fb8519fe9
Meco <920369182@qq.com>
2020-07-25 08:26:45 +0800

    Signed-off-by: Meco

--- rt-thread-3.1.3/bsp/stm32f103-msh-628/RTE/RTOS/rtconfig.h
+++ rt-thread-3.1.3/bsp/stm32f103-msh-628/RTE/RTOS/rtconfig.h
@@ -18,7 +18,7 @@
 #define RT_TICK_PER_SECOND  1000
 // <o>Alignment size for CPU architecture data access
 //  <i>Default: 4
-#define RT_ALIGN_SIZE   4
+#define RT_ALIGN_SIZE   8
 // <o>the max length of object name<2-16>
 //  <i>Default: 8
 #define RT_NAME_MAX    16
