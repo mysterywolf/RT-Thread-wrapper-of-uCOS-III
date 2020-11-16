36a02404cd9dbf621c8c53153b334ef00dfc1390
mysterywolf <920369182@qq.com>
2020-11-16 13:33:57 +0800

    增加在开启消息队列时检查是否开启内存堆的宏定义检查

--- uCOS-III/os.h
+++ uCOS-III/os.h
@@ -1590,12 +1590,16 @@ rt_err_t      rt_mq_send_all            (rt_mq_t mq, void *buffer, rt_size_t siz
 #warning "RT_USING_TIMER_SOFT宏没有开启,μCOS-III兼容层将无法启用软件定时器,如果您确认这么做的话请将本警告注释掉"
 #endif
 
+#if defined RT_USING_MESSAGEQUEUE && !defined RT_USING_HEAP
+#error "使用消息队列需要定义RT_USING_HEAP以使能堆内存功能"
+#endif
+
 #if OS_CFG_TASK_Q_EN && !OS_CFG_Q_EN
-#error "任务内建消息队列需要消息队列的支持，需要将OS_CFG_Q_EN置1方可使用"
+#error "任务内建消息队列需要消息队列的支持,需要将OS_CFG_Q_EN置1方可使用"
 #endif
 
 #if OS_CFG_TASK_SEM_EN && !OS_CFG_SEM_EN
-#error "任务内建信号量需要信号量的支持，需要将OS_CFG_SEM_EN置1方可使用"
+#error "任务内建信号量需要信号量的支持,需要将OS_CFG_SEM_EN置1方可使用"
 #endif
 
 #if OS_CFG_TASK_Q_PEND_ABORT_EN && !OS_CFG_Q_PEND_ABORT_EN
