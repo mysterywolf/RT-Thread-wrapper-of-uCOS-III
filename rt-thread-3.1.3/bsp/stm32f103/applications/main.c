413106a45de86de950b37786787ee067aa7e69c0
mysterywolf <920369182@qq.com>
2020-11-26 20:46:36 +0800

    update

--- rt-thread-3.1.3/bsp/stm32f103/applications/main.c
+++ rt-thread-3.1.3/bsp/stm32f103/applications/main.c
@@ -44,17 +44,17 @@ int main(void)/*RT-Thread main线程*/
     }
     
     /*创建开始任务*/
-	OSTaskCreate((OS_TCB 	* )&AppTaskStartTCB,		
-				 (CPU_CHAR	* )"App Task Start", 		
-                 (OS_TASK_PTR )AppTaskStart, 			
-                 (void		* )0,			
+    OSTaskCreate((OS_TCB 	* )&AppTaskStartTCB,
+                 (CPU_CHAR	* )"App Task Start", 
+                 (OS_TASK_PTR )AppTaskStart,
+                 (void		* )0,
                  (OS_PRIO	  )APP_TASK_START_PRIO,     
-                 (CPU_STK   * )&AppTaskStartStk[0],	
+                 (CPU_STK   * )&AppTaskStartStk[0],
                  (CPU_STK_SIZE)APP_TASK_START_STK_SIZE/10,
-                 (CPU_STK_SIZE)APP_TASK_START_STK_SIZE,	
-                 (OS_MSG_QTY  )0,					
-                 (OS_TICK	  )0,					
-                 (void   	* )0,					
+                 (CPU_STK_SIZE)APP_TASK_START_STK_SIZE,
+                 (OS_MSG_QTY  )0,
+                 (OS_TICK	  )0,
+                 (void   	* )0,
                  (OS_OPT      )OS_OPT_TASK_STK_CHK|OS_OPT_TASK_STK_CLR,
                  (OS_ERR 	* )&err);    
                  
